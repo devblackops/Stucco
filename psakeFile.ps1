d916ec90b2ccb4bec82cea54e816181348c62c46
Brandon Olin <brandon@devblackops.io>
2018-10-03 22:50:14 -0700

    Use shared psake tasks from PowerShellBuild.Common module

--- psakeFile.ps1
+++ psakeFile.ps1
@@ -1,11 +1,4 @@
 
 task default
 
-task Publish {
-    Publish-Module -Path ./Stucco -Repository PSGallery -NuGetApiKey $env:PSGALLERY_API_KEY
-} -description 'Publish module to PSGallery'
-
-task ? -description 'Lists the available tasks' {
-    'Available tasks:'
-    $psake.context.Peek().Tasks.Keys | Sort-Object
-}
+task Build -FromModule PowerShellBuild.Common -Version '0.1.0'
--- requirements.psd1
+++ requirements.psd1
@@ -2,5 +2,13 @@
     PSDependOptions = @{
         Target = 'CurrentUser'
     }
-    'psake' = 'latest'
+    'psake' = @{
+        Version = '4.8.0'
+    }
+    'BuildHelpers' = @{
+        Version = '2.0.0'
+    }
+    'PowerShellBuild.Common' = @{
+        Version = '0.1.0'
+    }
 }
