19d50492c7ad6f86917a59dc7a9c25fbc13ffd8f
Brandon Olin <brandon@devblackops.io>
2018-10-04 22:48:32 -0700

    Add example public and private functions

---
+++ Stucco/template/module/Private/priv.ps1
@@ -0,0 +1,3 @@
+function GetHelloWorld {
+    'Hello world'
+}
--- Stucco/template/module/Public/pub.ps1
+++ Stucco/template/module/Public/pub.ps1
@@ -1,14 +1,5 @@
 
-# Dot source public/private functions
-$public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'public/*.ps1')  -Recurse -ErrorAction Stop)
-$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'private/*.ps1') -Recurse -ErrorAction Stop)
-foreach ($import in @($public + $private)) {
-    try {
-        . $import.FullName
-    }
-    catch {
-        throw "Unable to dot source [$($import.FullName)]"
-    }
+Function Get-HelloWorld {
+    $value = GetHelloWorld
+    $value
 }
-
-Export-ModuleMember -Function $public.Basename
