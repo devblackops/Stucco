045386de13ed247c4832416b2241f6605e9d9079
Brandon Olin <brandon@devblackops.io>
2018-10-05 13:04:18 -0700

    Fix variable names

--- Stucco/template/tests/Manifest.tests.ps1
+++ Stucco/template/tests/Manifest.tests.ps1
@@ -1,7 +1,7 @@
 
 $moduleName         = $env:BHProjectName
 $manifest           = Import-PowerShellDataFile -Path $env:BHPSModuleManifest
-$outputDir          = Join-Path -Path $ENV:BHProjectPath -ChildPath 'out'
+$outputDir          = Join-Path -Path $ENV:BHProjectPath -ChildPath 'Output'
 $outputModDir       = Join-Path -Path $outputDir -ChildPath $env:BHProjectName
 $outputModVerDir    = Join-Path -Path $outputModDir -ChildPath $manifest.ModuleVersion
 $outputManifestPath = Join-Path -Path $outputModVerDir -Child "$($moduleName).psd1"
