c817fa56e753121e55764c98bd036425cb250922
Brandon Olin <brandon@devblackops.io>
2018-10-06 20:34:00 -0700

    Disable module "compilation" into monolithic PSM1

--- Stucco/template/psakeFile.ps1
+++ Stucco/template/psakeFile.ps1
@@ -1,3 +1,10 @@
+properties {
+    # Disable "compiling" module into monolithinc PSM1.
+    # This modifies the default behavior from the "Build" task
+    # in the PowerShellBuild.Common shared psake task module
+    $CompileModule = $false
+}
+
 task default
 
 task Build -FromModule PowerShellBuild.Common -Version '0.1.0'
