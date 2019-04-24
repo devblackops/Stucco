02f7b56183622b773715302dab0eadaef1ac3d44
Brandon Olin <brandon@devblackops.io>
2019-04-23 22:35:36 -0700

    Make default task depend on Test from shared module

--- Stucco/template/psakeFile.ps1
+++ Stucco/template/psakeFile.ps1
@@ -5,6 +5,6 @@ properties {
     $PSBPreference.Build.CompileModule = $false
 }
 
-task default
+task default -depends Test
 
-task Build -FromModule PowerShellBuild -Version '0.3.0'
+task Test -FromModule PowerShellBuild -Version '0.3.0'
