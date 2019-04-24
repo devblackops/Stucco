3b29433d721065b0cc8982c9b96af2ea0fc8098d
Brandon Olin <brandon@devblackops.io>
2019-04-23 22:35:16 -0700

    Add support for passing properties hashtable to psake

--- Stucco/template/build.ps1
+++ Stucco/template/build.ps1
@@ -9,7 +9,10 @@ param(
 
     # List available build tasks
     [parameter(ParameterSetName = 'Help')]
-    [switch]$Help
+    [switch]$Help,
+
+    # Optional properties to pass to psake
+    [hashtable]$Properties
 )
 
 $ErrorActionPreference = 'Stop'
@@ -18,21 +21,24 @@ $ErrorActionPreference = 'Stop'
 if ($Bootstrap.IsPresent) {
     Get-PackageProvider -Name Nuget -ForceBootstrap | Out-Null
     Set-PSRepository -Name PSGallery -InstallationPolicy Trusted
-    if (-not (Get-Module -Name PSDepend -ListAvailable)) {
-        Install-module -Name PSDepend -Repository PSGallery
+    if ((Test-Path -Path ./requirements.psd1)) {
+        if (-not (Get-Module -Name PSDepend -ListAvailable)) {
+            Install-Module -Name PSDepend -Repository PSGallery -Scope CurrentUser -Force
+        }
+        Import-Module -Name PSDepend -Verbose:$false
+        Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
+    } else {
+        Write-Warning "No [requirements.psd1] found. Skipping build dependency installation."
     }
-    Import-Module -Name PSDepend -Verbose:$false
-    Invoke-PSDepend -Path './requirements.psd1' -Install -Import -Force -WarningAction SilentlyContinue
 }
 
 # Execute psake task(s)
 $psakeFile = './psakeFile.ps1'
 if ($PSCmdlet.ParameterSetName -eq 'Help') {
-    Get-PSakeScriptTasks -buildFile $psakeFile  |
+    Get-PSakeScriptTasks -buildFile $psakeFile |
         Format-Table -Property Name, Description, Alias, DependsOn
 } else {
     Set-BuildEnvironment -Force
-
-    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo
-    exit ( [int]( -not $psake.build_success ) )
+    Invoke-psake -buildFile $psakeFile -taskList $Task -nologo -properties $Properties
+    exit ([int](-not $psake.build_success))
 }
