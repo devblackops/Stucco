357f8b009ef8f4fea4f27186d0e442993f212564
Brandon Olin <brandon@devblackops.io>
2021-02-27 14:30:51 -0800

    Fix class support
    Fixes #13

---
+++ Stucco/template/module/Module_classes.psm1
@@ -0,0 +1,13 @@
+# Dot source public/private functions
+$classes = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Classes/*.ps1') -Recurse -ErrorAction Stop)
+$public  = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Public/*.ps1')  -Recurse -ErrorAction Stop)
+$private = @(Get-ChildItem -Path (Join-Path -Path $PSScriptRoot -ChildPath 'Private/*.ps1') -Recurse -ErrorAction Stop)
+foreach ($import in @($classes + $public + $private)) {
+    try {
+        . $import.FullName
+    } catch {
+        throw "Unable to dot source [$($import.FullName)]"
+    }
+}
+
+Export-ModuleMember -Function $public.Basename
